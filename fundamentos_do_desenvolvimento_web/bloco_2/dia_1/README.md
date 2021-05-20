94671e357b1ab744f84d5910ceb3c7ae1b2398da
Antônio Gonçalves <tonygoldmarket@yahoo.com>
2021-05-20 15:18:07 -0300

    Inicio dos exercicios do bloco 4 - dia 2

---
+++ fundamentos_do_desenvolvimento_web/bloco_2/dia_1/README.md
@@ -0,0 +1,1 @@
+Exercício para aprender a fazer commits e pushs com Git e Github.
---
+++ fundamentos_do_desenvolvimento_web/bloco_4/dia_1/script.js
@@ -0,0 +1,18 @@
+let grade = 'xablau';
+
+switch (grade) {
+  case 'aprovada':
+    console.log('aprovada');
+    break;
+  
+  case 'lista':
+    console.log('lista de espera');
+    break;
+
+  case 'reprovada':
+    console.log('reprovada');
+    break
+
+  default:
+    console.log('não se aplica');
+}
