35a72a5031b088598dcd7659e16c47debad3f0a9
Antônio Gonçalves <tonygoldmarket@yahoo.com>
2021-05-19 23:58:22 -0300

    organizacao do repositorio

---
+++ exercises/bloco_4/dia_1/ex_1.js
@@ -0,0 +1,11 @@
+/* Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , definidas no começo com os valores que serão operados. Faça programas para:
+ */
+
+let num1 = 8;
+let num2 = 7;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
