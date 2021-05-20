d78134cda114cc9a6bd584eb11c41023f68cc6ae
Antônio Gonçalves <tonygoldmarket@yahoo.com>
2021-05-20 19:55:42 -0300

    Exercicios do bloco 4 - dia 2 concluidos

---
+++ fundamentos_do_desenvolvimento_web/bloco_4/dia_2/ex_8.js
@@ -0,0 +1,10 @@
+/* Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+ */
+
+let arr = [];
+
+for (index = 1; index <= 25; index += 1){
+  arr.push(index);
+}
+
+console.log(arr);
