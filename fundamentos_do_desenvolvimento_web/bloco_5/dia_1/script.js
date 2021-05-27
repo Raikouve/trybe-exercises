c69e3849a4f95c9628a52dd419c3ab7da77e52ed
Antônio Gonçalves <tonygoldmarket@yahoo.com>
2021-05-27 15:28:45 -0300

    Terminei os exemplos

---
+++ fundamentos_do_desenvolvimento_web/bloco_5/dia_1/script.js
@@ -0,0 +1,10 @@
+document.querySelector('#page-title').innerText = 'Scarface';
+
+document.querySelector('p');
+paragraph.style.color = 'blue';
+
+document.querySelector('#subtitle').innerText = 'Por que eu gosto?';
+
+document.querySelectorAll('.paragrapho')[1].style.color = 'red';
+
+document.querySelectorAll('h4')[0].style.color = 'green';
