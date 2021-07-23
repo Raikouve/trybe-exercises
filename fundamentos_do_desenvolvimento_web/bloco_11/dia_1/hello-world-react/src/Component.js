49970f051401500f46a6100bb5820d6d99f3fdd5
Antônio Gonçalves <tonygoldmarket@yahoo.com>
2021-07-23 02:03:36 -0300

    Primeiro Hello World/Primeiro Componente React!

---
+++ fundamentos_do_desenvolvimento_web/bloco_11/dia_1/hello-world-react/src/Component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class Component extends React.Component {
+  render() {
+    return <div className='Component'>
+      <h1>Antônio Gonçalves</h1>
+      <p>Aprendendo React</p>
+    </div>
+  }
+}
+
+export default Component;
