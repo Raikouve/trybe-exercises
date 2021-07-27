313f54b554c21f277b00239962a2ccd91549d989
Antônio Gonçalves <tonygoldmarket@yahoo.com>
2021-07-27 05:05:52 -0300

    Exemplos de estado - Fancy buttons

--- fundamentos_do_desenvolvimento_web/bloco_12/dia_1/fancy-buttons-example/src/App.js
+++ fundamentos_do_desenvolvimento_web/bloco_12/dia_1/fancy-buttons-example/src/App.js
@@ -1,25 +1,43 @@
 import React from 'react';
 import './App.css';
 
-function handleClick1 () {
-  return console.log('Botão 1 clicado')
-}
-
-function handleClick2 () {
-  return console.log('Botão 2 clicado')
-}
-
-function handleClick3 () {
-  return console.log('Botão 3 clicado')
-}
-
 class App extends React.Component {
+  constructor(props) {
+    // A função `super()` é chamada para garantir que a lógica interna do React rode **antes** da sua. Se não for assim, o código não funcionará
+
+    super()
+    // Sua lógica extra vai aqui! O parâmetro `props` é opcional, para você acessar as props diretamente no construtor
+
+    // A função abaixo vincula "manualmente" o `this` à nossa função
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      numberOfCLicks: 0,
+    }
+  }
+  handleClick () {
+    /* Agora esse log retorna o objeto `this`, já acessível para nossa função!
+    Com isso, podemos acessar as `props`, estado do componente (ainda vamos ver como!)
+    e tudo o mais daqui de dentro */
+
+    /* Você **NUNCA** deve fazer atribuições diretamente a `this.state`. Deve,
+    ao invés disso, SEMPRE utilizar a função `this.setState(novoEstado)` do
+    React*/
+
+    /* Passando uma callback à função setState, que recebe de parâmetros
+    o estado anterior e as props do componente, você garante que as atualizações
+    do estado acontecerão uma depois da outra! */
+    this.setState((previousState, _props) =>  ({
+      numberOfCLicks: previousState.numberOfCLicks + 1
+    }));
+    if(this.state.numberOfCLicks % 2 === 1) {
+      console.log('verde');
+    }
+  }
   render() {
   return (
-    <div className='buttons'>
-    <button onClick={handleClick1}>Botão 1</button>
-    <button onClick={handleClick2}>Botão 2</button>
-    <button onClick={handleClick3}>Botão 3</button>
+    <div className='btn-container'>
+    {/* Para LER o estado, você pode usar `this.state.chaveDoMeuEstado */}
+    <button className='num-btn' onClick={this.handleClick}>{this.state.numberOfCLicks}</button>
     </div>
   )
   }
