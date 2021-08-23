be056a6a438a98e0aa139a4d1269db1646d5de5c
Antônio Gonçalves <tonygoldmarket@yahoo.com>
2021-08-23 03:17:19 -0300

    Dad Jokes - ciclo de vida,renderizaçao condicional e loading

---
+++ fundamentos_do_desenvolvimento_web/bloco_13/dia_1/dad-joke/src/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import './App.css';
+import DadJoke from './DadJoke';
+
+export default class App extends Component {
+  render() {
+    return (
+      <DadJoke/>
+    )
+  }
+}
